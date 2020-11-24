a41986664aff1df85a130d5818a0c904f6f6ab17
CrossNox <ijmermet@gmail.com>
2020-11-24 19:49:23 -0300

    init repo

---
+++ reviews_microservice/api.py
@@ -0,0 +1,17 @@
+"""API module."""
+from flask_restx import Api
+from reviews_microservice import __version__
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+api = Api(prefix="/v1", version=__version__, validate=True)
+
+
+@api.errorhandler
+def handle_exception(error: Exception):
+    """When an unhandled exception is raised"""
+    message = "Error: " + getattr(error, 'message', str(error))
+    return {'message': message}, getattr(error, 'code', 500)
