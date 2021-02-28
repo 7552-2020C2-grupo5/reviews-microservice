7a0e95174593d57b643276b86483be0890dac911
CrossNox <ijmermet@gmail.com>
2021-02-28 19:58:08 -0300

    validate token

--- reviews_microservice/app.py
+++ reviews_microservice/app.py
@@ -2,13 +2,14 @@
 import logging
 from pathlib import Path
 
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 from flask_migrate import Migrate
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 from reviews_microservice.api import api
 from reviews_microservice.cfg import config
+from reviews_microservice.constants import DEFAULT_VERIFICATION_URL
 from reviews_microservice.models import db
 
 logger = logging.getLogger(__name__)
@@ -20,6 +21,32 @@ def fix_dialect(s):
         s = s.replace("postgres://", "postgresql://")
     s = s.replace("postgresql://", "postgresql+psycopg2://")
     return s
+
+
+def before_request():
+    excluded_paths = [
+        "/",
+        "/swaggerui/favicon-32x32.png",
+        "/swagger.json",
+        "/swaggerui/swagger-ui-standalone-preset.js",
+        "/swaggerui/swagger-ui-standalone-preset.js",
+        "/swaggerui/swagger-ui-bundle.js",
+        "/swaggerui/swagger-ui.css",
+        "/swaggerui/droid-sans.css",
+    ]
+    if config.env(default="DEV") == "DEV":
+        return
+    if request.path not in excluded_paths and request.method != "OPTIONS":
+        bookbnb_token = request.headers.get("BookBNB-Authorization")
+        if bookbnb_token is None:
+            return {"message": "BookBNB token is missing"}, 401
+
+        r = request.post(
+            config.token_verification_url(default=DEFAULT_VERIFICATION_URL)
+        )
+
+        if not r.ok:
+            return {"message": "Invalid BookBNB token"}, 401
 
 
 def create_app():
@@ -37,4 +64,5 @@ def create_app():
     )  # remove after flask-restx > 0.2.0 is released
     # https://github.com/python-restx/flask-restx/issues/230
     CORS(new_app)
+    new_app.before_request(before_request)
     return new_app
--- reviews_microservice/constants.py
+++ reviews_microservice/constants.py
@@ -1,1 +1,5 @@
 """Constant values and defaults used in multiple modules."""
+
+DEFAULT_VERIFICATION_URL = (
+    "https://tokens-microservice.herokuapp.com/v1/tokens/verification"
+)
